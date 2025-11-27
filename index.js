6161b34d508f3160896f1b56a3d7d0a7be018cd3
Anick Umurungi <umurungianick@gmail.com>
2025-11-27 16:33:04 +0200

    Initial commit: Add basic Node.js web app

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("My First Node.js Web App!");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
